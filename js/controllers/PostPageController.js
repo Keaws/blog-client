3769e5b5b5831a90e9e441b4d86d7c642030b042
Vitalii Kalchuk <v.sweak@gmail.com>
2015-05-30 16:32:30 +0300

    Formatted code, removed extra tags

--- js/controllers/PostPageController.js
+++ js/controllers/PostPageController.js
@@ -1,44 +1,44 @@
-app.controller('PostPageController', function($scope, BlogService, $routeParams) {
-	BlogService.getPost($routeParams.id).then(function(payload) {
-		$scope.post = payload.data;
-	});
+app.controller('PostPageController', function ($scope, BlogService, $routeParams) {
+    BlogService.getPost($routeParams.id).then(function (payload) {
+        $scope.post = payload.data;
+    });
 
-	BlogService.getComments($routeParams.id).then(function(payload) {
-		$scope.comments = payload.data;
-	});
+    BlogService.getComments($routeParams.id).then(function (payload) {
+        $scope.comments = payload.data;
+    });
 
-	$scope.postComment = function() {
-		BlogService.postComment({
-			"postId": $routeParams.id,
-			"commentSummary": $scope.summary,
-			"commentText": $scope.text
-		}).then(function(){
-			var comment = { 
-				"summary": $scope.summary,
-				"text": $scope.text,
-				"timestamp": Date.now()
-			};
+    $scope.postComment = function () {
+        BlogService.postComment({
+            "postId": $routeParams.id,
+            "commentSummary": $scope.summary,
+            "commentText": $scope.text
+        }).then(function () {
+            var comment = {
+                "summary": $scope.summary,
+                "text": $scope.text,
+                "timestamp": Date.now()
+            };
 
-			$scope.comments.push(comment);
-			$scope.commentForm.$setPristine();
-			$scope.summary = '';
-			$scope.text = '';
+            $scope.comments.push(comment);
+            $scope.commentForm.$setPristine();
+            $scope.summary = '';
+            $scope.text = '';
 
-			BlogService.getComments($routeParams.id).then(function(payload) {
-				$scope.comments = payload.data;
-			});
-		});	
-	};
+            BlogService.getComments($routeParams.id).then(function (payload) {
+                $scope.comments = payload.data;
+            });
+        });
+    };
 
-	$scope.deleteComment = function(index) {
-		BlogService.deleteComment({
-			"postId": $routeParams.id,
-			"commentId": $scope.comments[index].id,
-		}).then(function(){
-			BlogService.getComments($routeParams.id).then(function(payload) {
-				$scope.comments = payload.data;
-			});
-		});
-	};
+    $scope.deleteComment = function (index) {
+        BlogService.deleteComment({
+            "postId": $routeParams.id,
+            "commentId": $scope.comments[index].id,
+        }).then(function () {
+            BlogService.getComments($routeParams.id).then(function (payload) {
+                $scope.comments = payload.data;
+            });
+        });
+    };
 
 });
--- js/services/BlogService.js
+++ js/services/BlogService.js
@@ -1,71 +1,71 @@
-app.service('BlogService', function($http) {
+app.service('BlogService', function ($http) {
 
-	this.getPosts = function() {
-		return $http({
-			url: 'http://localhost:3003/api/posts?begin=0&length=10',
-			method: 'GET'
-		});
-	};
+    this.getPosts = function () {
+        return $http({
+            url: 'http://localhost:3003/api/posts?begin=0&length=10',
+            method: 'GET'
+        });
+    };
 
-	this.getPost = function(postId) {
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + postId,
-			method: 'GET'
-		});
-	};
+    this.getPost = function (postId) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + postId,
+            method: 'GET'
+        });
+    };
 
-	this.getComments = function(postId) {
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + postId + '/comments',
-			method: 'GET'
-		});
-	};
+    this.getComments = function (postId) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + postId + '/comments',
+            method: 'GET'
+        });
+    };
 
-	this.postComment = function(params) {
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + params.postId + '/comments',
-			method: 'POST',
-			data: '{"text":"'+ params.commentText +'","summary":"'+ params.commentSummary +'", "timestamp":"' + Date.now() + '"}',
-    		headers: {
-    			'Content-Type': 'application/json'
-    		}
-		});
-	};
+    this.postComment = function (params) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + params.postId + '/comments',
+            method: 'POST',
+            data: '{"text":"' + params.commentText + '","summary":"' + params.commentSummary + '", "timestamp":"' + Date.now() + '"}',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
 
-	this.deleteComment = function(params) {
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + params.postId + '/comments/' + params.commentId,
-			method: 'DELETE'
-		});
-	};
+    this.deleteComment = function (params) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + params.postId + '/comments/' + params.commentId,
+            method: 'DELETE'
+        });
+    };
 
-	this.addPost = function(params){
-		return $http({
-			url: 'http://localhost:3003/api/posts',
-			method: 'POST',
-			data: '{"text":"' + params.text + '","title":"' + params.title + '", "author":"' + params.author + '","timestamp":"' + Date.now() + '"}',
-			headers: {
-    			'Content-Type': 'application/json'
-    		}
-		});
-	};
+    this.addPost = function (params) {
+        return $http({
+            url: 'http://localhost:3003/api/posts',
+            method: 'POST',
+            data: '{"text":"' + params.text + '","title":"' + params.title + '", "author":"' + params.author + '","timestamp":"' + Date.now() + '"}',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
 
-	this.deletePost = function(postId){
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + postId,
-			method: 'DELETE'
-		});
-	};
+    this.deletePost = function (postId) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + postId,
+            method: 'DELETE'
+        });
+    };
 
-	this.editPost = function(params){
-		return $http({
-			url: 'http://localhost:3003/api/posts/' + params.postId,
-			method: 'PUT',
-			data: '{"text":"' + params.postText + '","title":"' + params.postTitle + '","author":"' + params.postAuthor + '"}',
-			headers: {
-    			'Content-Type': 'application/json'
-    		}
-		});
-	};
+    this.editPost = function (params) {
+        return $http({
+            url: 'http://localhost:3003/api/posts/' + params.postId,
+            method: 'PUT',
+            data: '{"text":"' + params.postText + '","title":"' + params.postTitle + '","author":"' + params.postAuthor + '"}',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
 
 });
